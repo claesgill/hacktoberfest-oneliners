40e93a60a65d1fa02aaee43abc8e2b24fc0189e9
James <60527897+Logicism81@users.noreply.github.com>
2020-10-14 16:37:42 +0800

    Added one-liners
    2 majors changes:
    1. Added list of numbers from 1 to 100 to define the variable "numbers" (Change limit using the range(n) function) 
    2. Added variable swap function one-liner
    
    1 minor change:
    1. Changed the variable "alphabet" to "alphabets"

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 # list of the alphabet (a-z)
-alphabet = [ chr(ord("a") + i) for i in range(26) ]
+alphabets = [ chr(ord("a") + i) for i in range(26) ]
+
+# list of numbers (1-100)
+numbers = list(range(101))
+
+# swap 2 variables
+alphabets, numbers = numbers, alphabets
