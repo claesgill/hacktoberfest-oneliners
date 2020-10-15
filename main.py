496fb0a3c36a40d890ac6e4f38ef0db8fe2ed0e1
James <60527897+Logicism81@users.noreply.github.com>
2020-10-15 20:01:46 +0800

    Added sorted_alphabet to line 4 of main.py
    Takes the reversed alphabets list and sorts it back into a list from a-z

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
 # list of the alphabet (a-z)
-alphabets = [ chr(ord("a") + i) for i in range(26) ]
-
-# list of the alphabet in reverse
-reversed_alphabets = alphabets[::-1]
-
-# list of numbers (1-100)
-numbers = list(range(101))
-
-# swap 2 variables
-alphabets, numbers = numbers, alphabets
+alphabet = [ chr(ord("a") + i) for i in range(26) ]
+reversed_alphabet = alphabet[::-1]
+sorted_alphabet = alphabet.sort()
