ab34dab60fe3256460f7fe5e75b1618c48954f01
BudiRahmawan6507 <55727141+BudiRahmawan@users.noreply.github.com>
2020-10-16 17:40:54 +0700

    Update main.py
    concatenate numbers and alphabets into 1 array (0-9,a-z)

--- main.py
+++ main.py
@@ -2,3 +2,5 @@
 alphabet = [ chr(ord("a") + i) for i in range(26) ]
 reversed_alphabet = alphabet[::-1]
 sorted_alphabet = alphabet.sort()
+#concatenate numbers and alphabets into 1 array (0-9,a-z)
+numbers_alphabet = [*(chr(ord("0") + i) for i in range(10)), *alphabet]
