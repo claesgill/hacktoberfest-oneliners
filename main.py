812b48f38a0903b78cddcf24d4e531e626950928
Claes Gill <claes@claesgill.com>
2020-10-17 12:54:09 +0200

    Placed comments after line

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-# list of the alphabet (a-z)
-alphabet = [ chr(ord("a") + i) for i in range(26) ]
+alphabet = [ chr(ord("a") + i) for i in range(26) ] # list of the alphabet (a-z)
 reversed_alphabet = alphabet[::-1]
 sorted_alphabet = alphabet.sort()
-#concatenate numbers and alphabets into 1 array (0-9,a-z)
-numbers_alphabet = [*(chr(ord("0") + i) for i in range(10)), *alphabet]
+numbers_alphabet = [*(chr(ord("0") + i) for i in range(10)), *alphabet] # concatenate numbers and alphabets into 1 array (0-9,a-z)
