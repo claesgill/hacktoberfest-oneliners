58f222d23c0d9cbe01d1d864646e30b7b58b38a2
Claes Gill <claes@claesgill.com>
2020-10-19 11:46:42 +0200

    Adding test branch to explore badge

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-alphabet = [ chr(ord("a") + i) for i in range(26) ] # list of the alphabet (a-z)
+alphabet  [ chr(ord("a") + i) for i in range(26) ] # list of the alphabet (a-z)
 reversed_alphabet = alphabet[::-1]
 sorted_alphabet = alphabet.sort()
 numbers_alphabet = [*(chr(ord("0") + i) for i in range(10)), *alphabet] # concatenate numbers and alphabets into 1 array (0-9,a-z)
