acb95654076aa5fdecd2f25d0cde44836209ad0c
Claes Gill <claes@claesgill.com>
2023-10-18 19:11:13 +0200

    Numberd list with alphabet multiplied all numbers with the meaning of life, 42.

--- main.py
+++ main.py
@@ -2,3 +2,4 @@ alphabet = [ chr(ord("a") + i) for i in range(26) ] # list of the alphabet (a-z)
 reversed_alphabet = alphabet[::-1]
 sorted_alphabet = alphabet.sort()
 numbers_alphabet = [*(chr(ord("0") + i) for i in range(10)), *alphabet] # concatenate numbers and alphabets into 1 array (0-9,a-z)
+numbers42_alphabet = [(int(i) * 42) for i in numbers_alphabet[0:9]] + alphabet
