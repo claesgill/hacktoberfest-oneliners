2b4e598c7501d6bb5635d2796c079030ef06e0a3
Angela-zw <146141016+Angela-zw@users.noreply.github.com>
2023-10-18 20:22:28 -0700

    Capital letters

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ reversed_alphabet = alphabet[::-1]
 sorted_alphabet = alphabet.sort()
 numbers_alphabet = [*(chr(ord("0") + i) for i in range(10)), *alphabet] # concatenate numbers and alphabets into 1 array (0-9,a-z)
 numbers42_alphabet = [(int(i) * 42) for i in numbers_alphabet[0:9]] + alphabet
+alphabet_capatalized = [*(chr(ord("A") + i) for i in range(26))]
